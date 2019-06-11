d79834e75fccf448dfd7c6c8ded50cc0c3db6fb9
Nigel Alford <ngalford@gmail.com>
2019-06-11 12:19:26 -0400

    starting interactive messaging, updated helper api

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 require('dotenv').config();
-const request = require('request');
 const Firestore = require('@google-cloud/firestore');
 const db = require('./lib/db');
 const helper = require('./lib/helpers');
-
-const COLLECTION_NAME = process.env.FIREBASE_COLLECTION;
 const firestore = new Firestore();
-
-function query(teamId, docId) {
-  return firestore
-    .collection('teams')
-    .doc(teamId)
-    .collection('coffee')
-    .doc(docId)
-    .get()
-    .then(records => {
-      if (records.empty) {
-        return [];
-      } else {
-        return records.docs.map(result => result.data());
-      }
-    });
-}
 
 /**
  * HTTP Cloud Function.
@@ -43,93 +24,65 @@ exports.coffeeHTTP = (req, res) => {
       var id = data.actions[0].value;
       var actionName = data.actions[0].name;
       var teamId = data.team.id;
+      var channel = data.channel.id;
+      var response_url = data.response_url;
+
       const firestoreRef = firestore
         .collection('teams')
         .doc(teamId)
         .collection('coffee')
         .doc(id);
 
-      // const name = query(teamId, id).then(doc => {
-      //   if(doc) {
-      //     return itemName = doc[0].name
-      //   }
-      // });
+      res.status(200).send('');
 
       switch (actionName) {
         case 'like':
           return firestoreRef
             .update({ likes: increment })
-            .then(() => {
-              return res
-                .status(200)
-                .send(
-                  `thanks, your like has been noted, I'll try to keep that coffee around for you`
-                );
-            }) // send a slack formatted message back
-            .catch(err =>
-              res.status(404).send({ error: 'unable to store', err })
+            .then(() =>
+              helper.replaceMessage({
+                response_url,
+                text: `thanks, your like has been noted, I'll try to keep that coffee around for you`,
+              })
+            ) // send a slack formatted message back
+            .catch((err) =>
+              helper.replaceMessage({
+                response_url,
+                text: err,
+              })
             );
         case 'dislike':
           return firestoreRef
             .update({ dislikes: increment })
             .then(() =>
-              res
-                .status(200)
-                .send(
-                  "thanks, your dislike has been noted, I'll work on getting less of that coffee "
-                )
+              helper.replaceMessage({
+                response_url,
+                text:
+                  "thanks, your dislike has been noted, I'll work on getting less of that coffee ",
+              })
             )
-            .catch(err =>
-              res.status(404).send({ error: 'unable to store', err })
+            .catch((err) =>
+              helper.replaceMessage({
+                response_url,
+                text: err,
+              })
             );
         case 'brew':
           return firestoreRef
             .update({ brew_date: Date.now() })
             .then(() =>
-              res
-                .status(200)
-                .send('excellent, I love when more coffee is brewed')
+              helper.replaceMessage({
+                response_url,
+                text: 'excellent, I love when more coffee is brewed',
+              })
             )
-            .catch(err =>
-              res.status(404).send({ error: 'unable to store', err })
-            );
-        default:
-          return firestoreRef
-            .set({
-              id,
-              name: data.name,
-              roaster: data.roaster,
-              brew_date: null,
-              likes: 0,
-              dislikes: 0,
-            })
-            .then(() => {
-              return res.status(200).send(doc);
-            })
-            .catch(err =>
-              res.status(404).send({ error: 'unable to store', err })
+            .catch((err) =>
+              helper.replaceMessage({
+                response_url,
+                text: err,
+              })
             );
       }
-    case 'GET': // read/retrieve an existing document by id
-      if (!(req.query && req.query.id)) {
-        return res.status(404).send({ error: 'No Id' });
-      }
-      var id = req.query.id.replace(/[^a-zA-Z0-9]/g, '').trim();
-      if (!(id && id.length)) {
-        return res.status(404).send({ error: 'Empty Id' });
-      }
-      return firestore
-        .collection(COLLECTION_NAME)
-        .doc(id)
-        .then(doc => {
-          if (!(doc && doc.exists)) {
-            return res
-              .status(404)
-              .send({ error: 'Unable to find the document' });
-          }
-          return res.status(200).send(doc.data());
-        })
-        .catch(err => res.status(404).send({ error: err }));
   }
 };
 
@@ -145,24 +98,23 @@ exports.coffeeEvents = (req, res) => {
       });
   }
 
-  // send body to factory for processing
-  res.sendStatus(200);
+  res.status(200).send('');
 
   // improve this protection
   if (payload.event.type !== 'message' && !payload.event.text) {
     return false;
   }
   const text = payload.event.text;
-  const channel = req.body.event.channel;
+  const channel = payload.event.channel;
   const teamId = payload.team_id;
 
   if (text && text.match(/(sup)/gi)) {
-    postMessage({ text: helper.greeting.whatsUp(), channel: channel });
+    helper.postMessage({ text: helper.greeting.whatsUp(), channel: channel });
   }
 
   if (text && text.match(/(add coffee: )/gi)) {
-    addCoffee(text, teamId).then(coffee =>
-      postMessage({
+    helper.addCoffee(text, teamId).then((coffee) =>
+      helper.helper.postMessage({
         text: 'congrats *' + coffee.name + '* was saved, lets pour a cup now!',
         channel: channel,
       })
@@ -170,86 +122,27 @@ exports.coffeeEvents = (req, res) => {
   }
 
   if (text && text.match(/(menu)/g)) {
-    postMessage({
+    helper.postMessage({
       text:
         'searching while drinking coffee, nothing to see here or worry about...',
       channel: channel,
     });
 
-    db.getTodaysBrewedCoffee(teamId).then(coffee => {
+    db.getTodaysBrewedCoffee(teamId).then((coffee) => {
       count = 0;
-      if (coffee) {
-        return postMessage({
+      if (coffee.length > 0) {
+        const attachments = helper.setSlackResponse(coffee);
+        helper.postMessage({
           channel: channel,
           text: "Here's what I found",
-          attachments: JSON.stringify(helper.setSlackResponse(coffee))
+          attachments: JSON.stringify(attachments),
         });
       } else {
-        return postMessage({
+        helper.postMessage({
           text: 'no coffee found, brew up and try again',
           channel: channel,
         });
       }
     });
-  }
-
-  function postMessage(msg) {
-    if(msg.text) {
-      request.post(
-        {
-          url: 'https://slack.com/api/chat.postMessage',
-          form: {
-            token: process.env.SLACK_TOKEN,
-            text: msg.text,
-            channel: msg.channel,
-            attachments: msg.attachments,
-            blocks: msg.blocks
-          },
-        },
-        (err, res, body) => {
-          if (err) {
-            console.error('error: ', err);
-          }
-          if (res) {
-            console.log('sent: ', res);
-          }
-          if (body) {
-            console.log('body: ', body);
-          }
-        }
-      );
-
-    }
-    //
-  }
-
-  function addCoffee(msg, teamId) {
-    var name = msg
-      .substring(msg.lastIndexOf('coffee: ') + 7, msg.lastIndexOf('by: '))
-      .trim();
-    var roaster = msg
-      .substring(msg.lastIndexOf('by: ') + 4, msg.lastIndexOf('url'))
-      .trim();
-    var url = msg
-      .substring(msg.lastIndexOf('url: ') + 5, msg.length)
-      .replace(/[<>]/g, '')
-      .trim();
-
-    var newFood = {
-      docId: teamId,
-      subCollection: 'coffee',
-      id: name
-        .concat(roaster)
-        .replace(/ /g, '')
-        .toLowerCase(),
-      name,
-      roaster,
-      brew_date: Date.now(),
-      likes: 0,
-      dislikes: 0,
-      url: url,
-    };
-
-    return db.nestedSave(newFood, teamId).then(pantryItem => pantryItem);
   }
 };
